becd1bf11c66dc99ac8e7855a45f4dbf02092aa6
Andrew Williams <1williamsaj@gmail.com>
2019-10-13 19:44:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,25 +5,25 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
   @@artists = []
 
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
+  def self.all
+    @@artists
+  end
 
   def initialize
     super
     @songs = []
   end
 
-  def self.all
-    @@artists
-  end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
