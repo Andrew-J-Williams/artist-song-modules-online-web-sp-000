751aedfddf4f32c9d2ab06014f1580d91da1ce18
Andrew Williams <1williamsaj@gmail.com>
2019-10-13 19:30:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+#require_relative '../lib/concerns/memorable'
 require 'pry'
 
 class Artist
@@ -6,25 +7,17 @@ class Artist
 
   @@artists = []
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+  extend Memorable
+  extend Findable
+  include Paramable
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+  def initialize
+    self.class.all << self
+    @songs = []
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -36,7 +29,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+#require_relative '../lib/concerns/memorable'
 require 'pry'
 
 class Song
@@ -6,31 +7,20 @@ class Song
 
   @@songs = []
 
+  extend Memorable
+  extend Findable
+  include Paramable
+
   def initialize
     @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
   end
 
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
   def artist=(artist)
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
